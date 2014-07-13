6490a10c47a7d5895bab4d1721a1bc87b34987ee
Yibo-Li <gansuliyibo@126.com>
2014-07-13 15:48:47 +0800

    完成设计项目4

---
+++ 04-2.asm
@@ -0,0 +1,98 @@
+assume cs:code
+data segment
+    dw 2,15,0
+data ends
+stack segment
+    dw 8 dup (0)
+stack ends
+code segment
+start:
+;使用add计算乘方
+mov ax,stack
+mov ss,ax
+mov sp,16
+mov ax,data
+mov ds,ax
+mov bx,0
+mov cx,[bx+2]
+mov dl,[bx]
+mov bx,dx
+mov al,1
+mov ah,0
+s1:
+push cx
+mov cx,bx
+mov dx,ax
+mov ax,0
+s:
+add ax,dx
+loop s
+pop cx
+loop s1
+mov dx,ax
+
+;显示结果
+mov ax,0b878h
+mov ds,ax
+mov bx,288h
+mov ax,dx
+mov dx,0
+mov cx,5
+s5:
+push cx
+mov cx,10
+div cx
+add dl,30h
+mov [bx],dl
+sub bx,2
+mov dx,0
+pop cx
+loop s5
+
+mov bx,8h
+mov ax,data
+mov ss,ax
+mov sp,0
+pop ax
+mov dx,0
+mov cx,5
+s3:
+push cx
+mov cx,10
+div cx
+add dl,30h
+mov [bx],dl
+sub bx,2
+mov dx,0
+pop cx
+loop s3
+
+mov bx,0a0h
+mov byte ptr [bx],'^'
+
+mov bx,148h
+pop ax
+mov dx,0
+mov cx,5
+s4:
+push cx
+mov cx,10
+div cx
+add dl,30h
+mov [bx],dl
+sub bx,2
+mov dx,0
+pop cx
+loop s4
+
+mov bx,1e0h
+mov byte ptr [bx+0h],'-'
+mov byte ptr [bx+2h],'-'
+mov byte ptr [bx+4h],'-'
+mov byte ptr [bx+6h],'-'
+mov byte ptr [bx+8h],'-'
+
+mov ax,4c00h
+int 21h
+code ends
+end start
